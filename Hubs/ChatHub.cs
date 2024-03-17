feeabe242ad6181e044c4472088aadb3826bfe16
João Vitor Ricci Pironte <50086474+Pironte@users.noreply.github.com>
2024-03-17 13:50:08 -0300

    Removendo toda a implementação do ChatHub

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -5,68 +5,10 @@ namespace Comigle.Hubs
 {
     public class ChatHub : Hub
     {
-        public static ConcurrentDictionary<string, string> peersDic;
-
-        public static ConcurrentDictionary<string, string> peersDicConnect;
-
         public async Task Send(string connectionId, string message)
         {
             Console.WriteLine(message);
             await Clients.Others.SendAsync("Receive", connectionId, message);
-        }
-
-        public async Task SendToPeer(string connectionId, string message)
-        {
-            Console.Write(message);
-            var client = Clients.Client(connectionId);
-            await client.SendAsync("Receive", connectionId, message);
-        }
-
-        public void SavePeersDictionary(string connectionId, string state)
-        {
-            if (peersDic == null)
-                peersDic = new ConcurrentDictionary<string, string>();
-
-            if (string.IsNullOrEmpty(state))
-                state = "new";
-
-            peersDic[connectionId] = state;
-        }
-
-        public void SavePeersConnect(string localConnectionId, string remoteConnectionId)
-        {
-            if (peersDicConnect == null)
-                peersDicConnect = new ConcurrentDictionary<string, string>();
-
-            peersDicConnect[localConnectionId] = remoteConnectionId;
-        }
-
-        public void DeletePeersConnect(string connectionId, string state)
-        {
-            var remoteConnectionid = peersDicConnect[connectionId];
-            peersDicConnect.TryRemove(connectionId, out var peers);
-            peersDicConnect.TryRemove(remoteConnectionid, out peers);
-        }
-
-        public string GetPeersConnect(string connectionId)
-        {
-            if (peersDicConnect == null)
-                peersDicConnect = new ConcurrentDictionary<string, string>();
-
-            if (peersDicConnect.ContainsKey(connectionId))
-                return peersDicConnect[connectionId];
-
-            return string.Empty;
-        }
-
-        public void DeletePeersDictionary(string connectionId, string state)
-        {
-            peersDic.TryRemove(connectionId, out var peers);
-        }
-
-        public string GetSateOfPeer(string connectionId)
-        {
-            return peersDic[connectionId];
         }
     }
 }
